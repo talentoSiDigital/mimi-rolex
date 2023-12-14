f6437e03b431a00386c612ba716cccfbb457a3d5
Manuel Zorrilla <manuelzorrilladev@gmail.com>
2023-12-14 19:32:52 -0400

    Initial commit, created server, controller and models

---
+++ rolex-client/src/main.js
@@ -0,0 +1,58 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+/* import router from './router' */
+
+import VueCookieAcceptDecline from 'vue-cookie-accept-decline';
+import 'vue-cookie-accept-decline/dist/vue-cookie-accept-decline.css';
+
+
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import {
+    faMagnifyingGlass,
+    faAngleDown,
+    faIndustry,
+    faHandshake,
+    faAward,
+    faHandsHoldingCircle,
+    faCircleCheck,
+    faEnvelope,
+    faPhone,
+    faLocationDot,
+    faXmark, faAnglesDown, faBackward
+} from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF, faInstagram, faXTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+
+
+library.add(
+    faMagnifyingGlass,
+    faFacebookF,
+    faInstagram,
+    faAngleDown,
+    faIndustry,
+    faHandshake,
+    faAward,
+    faHandsHoldingCircle,
+    faCircleCheck,
+    faEnvelope,
+    faPhone,
+    faLocationDot,
+    faXTwitter,
+    faXmark, faWhatsapp, faAnglesDown, faBackward
+)
+
+
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+/*     .use(router) */
+    .component('vue-cookie-accept-decline', VueCookieAcceptDecline)
+    .mount('#app')
---
+++ server/app/models/index.js
@@ -0,0 +1,21 @@
+const dbConfig = require("../config/db.config.js");
+const Sequelize = require("sequelize");
+
+
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+
+  
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./model.js")(sequelize, Sequelize);
+
+module.exports = db;
